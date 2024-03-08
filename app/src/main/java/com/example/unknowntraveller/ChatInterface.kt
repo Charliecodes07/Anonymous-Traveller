5cec386d1ef0374798c72a049cd5d6a12b6f476e
Gaurav <garsule1234@gmail.com>
2024-03-09 01:47:07 +0530

    on-process

---
+++ app/src/main/java/com/example/unknowntraveller/ChatInterface.kt
@@ -0,0 +1,11 @@
+package com.example.unknowntraveller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatInterface : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_interface)
+    }
+}
