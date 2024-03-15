e3f88f552a02639ef924ad7b77e8f927154df2b0
Gaurav <garsule1234@gmail.com>
2024-03-15 19:06:12 +0530

    UI Update

--- app/src/main/java/com/example/unknowntraveller/ChatInterface.kt
+++ app/src/main/java/com/example/unknowntraveller/ChatInterface.kt
@@ -3,6 +3,9 @@ package com.example.unknowntraveller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
+
 class ChatInterface : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
